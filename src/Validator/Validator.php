5d4217ee261416db616af8ad1e070701e9b383bf
zhengzean <andyzheng1024@gmail.com>
2016-07-24 17:31:08 +0800

    support Clousure and contains two filter

--- src/Validator/Validator.php
+++ src/Validator/Validator.php
@@ -1,6 +1,8 @@
 <?php
 namespace Andy\Validator;
 
+use Closure;
+
 /**
  *
  */
@@ -10,18 +12,84 @@ class Validator
 
     protected $rules;
 
+    protected $expectedRules;
+
     public function __construct(array $data, array $rules)
     {
         $this->data = $data;
         $this->rules = $rules;
     }
 
-    public function validate()
+    public static function validate(array $data, array $rules)
     {
-
+        $validator = new self($data, $rules);
+        return $validator->doValidate();
     }
-    //TODO
-    //1、分析rule的方法，并存储到一个属性里
-    //2、分析是否是默认的判定规则，如果是，则执行相关校验方法，不是则根据对应的类或者callback去处理
-    //3、定义interface，作为过滤器统一接口
+
+    protected function doValidate()
+    {
+        $this->analysisRules();
+        $result = [];
+        foreach ($this->expectedRules as $key => $ruleArray) {
+            $result[$key] = true;
+            foreach ($ruleArray as $rule) {
+                if ($rule['filter'] instanceof Closure) {
+                    $result[$key] = $result[$key] && $rule['filter']($this->data[$key]);
+                } else {
+                    $filter = $rule['filter'];
+                    $params = $rule['params'];
+                    $result[$key] = $result[$key] && $this->$filter($this->data, $key, $params);
+                }
+            }
+        }
+        return $result;
+    }
+
+    protected function analysisRules()
+    {
+        $this->expectedRules = [];
+        foreach ($this->rules as $key => $rule) {
+            if ($rule instanceof Closure) {
+                $this->expectedRules[$key][] = [
+                    'filter' => $rule
+                ];
+            } else {
+                $ruleItems = explode('|', $rule);
+                foreach ($ruleItems as $item) {
+                    if (empty($item)) {
+                        continue;
+                    }
+                    $detail = explode(':', $item);
+                    $this->expectedRules[$key][] = [
+                        'filter' => $detail[0],
+                        'params' => empty($detail[1]) ? [] : explode(',', $detail[1])
+                    ];
+                }
+            }
+        }
+    }
+
+    protected function required($data, $key, $params = null)
+    {
+        if (!array_key_exists($key, $data)) {
+            return false;
+        }
+        if (empty($data[$key])) {
+            return false;
+        }
+        return true;
+    }
+
+    protected function length($data, $key, $params)
+    {
+        $count = count($params);
+        if ($count === 1) {
+            return strlen($data[$key]) === intval($params[0]);
+        } elseif ($count === 2) {
+            if (strlen($data[$key]) >= $params[0] && strlen($data[$key]) < $params[1]) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
